3967683e2fa4537c4b711d2e300f2ba1c04a34bf
Scott Saltzman <salty923@gmail.com>
2018-02-24 09:47:33 -0600

    add path and body-parser

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 var PORT = 3000;
 
+var bodyParser = require("body-parser");
+var path = require("path");
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
