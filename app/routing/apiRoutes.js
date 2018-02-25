302e3ad94520f321596a28478ac4ad8c21d088bc
Scott Saltzman <salty923@gmail.com>
2018-02-24 21:52:59 -0600

    added user object on submit

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,7 @@
+app.get('/api/friends', (req, res) => {
+    
+});
+
+app.post('/api/firends', (req, res) => {
+    
+});
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var app = express();
 var PORT = 3000;
-
 var bodyParser = require("body-parser");
 var path = require("path");
 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
