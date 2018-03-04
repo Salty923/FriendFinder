cc83701907b678a1c11cb5853a11cfc344883a84
Scott Saltzman <salty923@gmail.com>
2018-03-03 22:01:17 -0600

    find friend logic wortking

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,10 @@
 
 
-var thisUser;
 var match;
 var allFriends = require('../data/friends');
-
+var thisUser;
+var allScores =[];
+var index;
 
 module.exports = function(app){
     //Shows user data at this url
@@ -16,23 +17,47 @@ module.exports = function(app){
         console.log("the post api/friends was hit");
         //pushing request body into usertable array in this file.
         thisUser = req.body;
-        console.log(thisUser); 
         allFriends.push(thisUser);
         res.send(JSON.stringify(allFriends));
-        
+        findFriend();
     });
-
-
-
-    //add logic here..
-    //for loop   this.User answers check
-    // for (let i = 0; i < allFriends.length; i++) {
-    //     thisUser.answer[i]
         
-    // }
-    
-
-}
+    }
 
 
+function findFriend() {
+    //var allScores =[];
+    let score =[];
+    let tally = 0;
+    //Loop through all user but not the last one which is current user
+    for (let i = 0; i < allFriends.length-1; i++) {
+        var compareUser = allFriends[i];
+        //loop through all of the question and push delta to score
+        for (let j = 0; j < 10; j++) {
+            let deltas= Math.abs(thisUser.answers[j] - compareUser.answers[j]);
+            score.push(deltas);         
+        }
+            //console.log(score);   
+        //add up sum of all 10 deltas in the scores array.
+        for (let k = 0; k < 11; k++) {
+            tally += score[i];
+            //console.log(score[i]);
+        }
+    allScores.push(tally);
+    tally = 0;
+    score = [];
+    }
+    console.log(allScores);
+};    
 
+
+// function lowest() {
+//     index = 0;
+//     var value = allScores[0];
+//     for (var l = 1; l < allScores.length; l++) {
+//         if (allScores[l] < value) {
+//             value = allScores[l];
+//             index = l;
+//         }
+//     }
+// }
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3001;
 
+//Use the express statc middlw ware to server static content for the app from the "public directory";
+app.use(express.static('app/public'));
+
 var bodyParser = require("body-parser");
 var path = require("path");
 
